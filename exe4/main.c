b210073c19ef23f3eb0676738909fcb81eae615b
karyne <karyneam@al.insper.edu.br>
2026-02-20 10:41:11 -0300

    ex4

--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,58 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTNR_PIN = 28;
+const int LED_R = 5;
+const int LED_P = 8;
+const int LED_B = 11;
+const int LED_Y = 15;
+
 int main() {
   stdio_init_all();
+  gpio_init(BTNR_PIN);
+  gpio_set_dir(BTNR_PIN, GPIO_IN);
+  gpio_pull_up(BTNR_PIN);
+  gpio_init(LED_R);
+  gpio_set_dir(LED_R, GPIO_OUT);
+  gpio_init(LED_P);
+  gpio_set_dir(LED_P, GPIO_OUT);
+  gpio_init(LED_B);
+  gpio_set_dir(LED_B, GPIO_OUT);
+  gpio_init(LED_Y);
+  gpio_set_dir(LED_Y, GPIO_OUT);
 
   while (true) {
-    // Use delay de 300 ms entre os estados!
+    if(!gpio_get(BTNR_PIN)){
+      gpio_put(LED_R, 1);
+      gpio_put(LED_P, 0);
+      gpio_put(LED_B, 0);
+      gpio_put(LED_Y, 0);
+      sleep_ms(300);
+
+      gpio_put(LED_R, 0);
+      gpio_put(LED_P, 1);
+      gpio_put(LED_B, 0);
+      gpio_put(LED_Y, 0);
+      sleep_ms(300);
+
+      gpio_put(LED_R, 0);
+      gpio_put(LED_P, 0);
+      gpio_put(LED_B, 1);
+      gpio_put(LED_Y, 0);
+      sleep_ms(300);
+
+      gpio_put(LED_R, 0);
+      gpio_put(LED_P, 0);
+      gpio_put(LED_B, 0);
+      gpio_put(LED_Y, 1);
+      sleep_ms(300);
+
+      gpio_put(LED_R, 0);
+      gpio_put(LED_P, 0);
+      gpio_put(LED_B, 0);
+      gpio_put(LED_Y, 0);
+      sleep_ms(300);
+    }
+
   }
 }
